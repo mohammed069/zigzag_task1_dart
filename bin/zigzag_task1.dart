b29073285d199ee090a3794058cae335884ec4b3
mohammed elshafey <126405959+mohammed069@users.noreply.github.com>
2025-02-04 19:17:56 +0200

    Initial commit

---
+++ bin/zigzag_task1.dart
@@ -0,0 +1,117 @@
+import 'dart:io';
+
+// //point 1
+// void main() {
+//   int number = 9;
+//   if (number % 2 == 0) {
+//     print("$number is even.");
+//   } else {
+//     print("$number is odd.");
+//   }
+// }
+
+// //point 2
+// void main() {
+//   String char = 'a';
+//   char = char.toLowerCase();
+//
+//   if ('aeiou'.contains(char)) {
+//     print("$char is a vowel.");
+//   } else {
+//     print("$char is a consonant.");
+//   }
+// }
+
+// //point 3
+// void main() {
+//   int number = 5;
+//   if (number > 0) {
+//     print("$number is positive.");
+//   } else if (number < 0) {
+//     print("$number is negative.");
+//   } else {
+//     print("The number is zero.");
+//   }
+// }
+
+// //point 4
+// void main() {
+//   for (int i = 0; i < 100; i++) {
+//     print("Mohammed ElShafey");
+//   }
+// }
+
+// //point 5
+// void main() {
+//   int n = 10;
+//   double sum = (n * (n + 1)) / 2;
+//   print("Sum of first $n natural numbers is ${sum.round()}.");
+// }
+
+// //point 6
+// void main() {
+//   for (int i = 1; i <= 10; i++) {
+//     print("5 x $i = ${5 * i}");
+//   }
+// }
+
+// //point 7
+// void main() {
+//   for (int i = 1; i <= 9; i++) {
+//     print("Multiplication Table of $i:");
+//     for (int j = 1; j <= 10; j++) {
+//       print("$i x $j = ${i * j}");
+//     }
+//     print("");
+//   }
+// }
+
+// //point 8
+// void main() {
+//   print("Enter first number:");
+//   double num1 = double.parse(stdin.readLineSync()!);
+//
+//   print("Enter operator (+, -, *, /):");
+//   String operator = stdin.readLineSync()!;
+//
+//   print("Enter second number:");
+//   double num2 = double.parse(stdin.readLineSync()!);
+//
+//   double result;
+//   switch (operator) {
+//     case '+':
+//       result = num1 + num2;
+//       break;
+//     case '-':
+//       result = num1 - num2;
+//       break;
+//     case '*':
+//       result = num1 * num2;
+//       break;
+//     case '/':
+//       result = num2 != 0 ? num1 / num2 : double.nan;
+//       break;
+//     default:
+//       print("Invalid operator.");
+//       return;
+//   }
+//   print("Result: $result");
+// }
+
+// //point 9
+// void main() {
+//   for (int i = 1; i <= 100; i++) {
+//     if (i == 41)
+//       continue;
+//     print(i);
+//   }
+// }
+
+
+
+
+
+
+
+
+
